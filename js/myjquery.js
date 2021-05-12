820bed7cc788c7a1403d4096ec6fe2bebab99bd8
Ailu Blue <ailublue@Ailus-MacBook-Pro.local>
2021-05-12 01:18:43 -0300

    First commit

---
+++ js/myjquery.js
@@ -0,0 +1,14 @@
+// inicio MENU DESPLEGABLE 
+
+$('#menu-toggle').click(function(){
+    $(this).toggleClass('open');
+  });
+  
+  
+  
+  $('#menu-toggle').click(function(){
+      $('nav , .redes').toggleClass('abierto');
+  });
+  
+   
+  // fin MENU DESPLEGABLE

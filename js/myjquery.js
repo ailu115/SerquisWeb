b93604227fa168d16663dd6c18f4c0b2ad7b3886
ailublue <32265483+ailu115@users.noreply.github.com>
2021-06-09 12:47:49 -0300

    algunos estilos nuevos

--- js/myjquery.js
+++ js/myjquery.js
@@ -31,3 +31,20 @@ $('#menu-toggle').click(function(){
       moreText.style.display = "inline";
     }
   }
+
+
+  // Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const trailhead = { lat: 35.674880, lng: -105.961820 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: trailhead,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: trailhead,
+    map: map,
+  });
+}
